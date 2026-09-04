225f8f5f37501447cd81c6d335b630867a986659
pyrimidine <3223145991@qq.com>
2026-09-04 23:22:24 +0800

    建立一个合适的udf

---
+++ head/udf.h
@@ -0,0 +1,8 @@
+#define FAR_FIELD(p,Ma,T,AOA)
+    extern void farfield_boundary(double p,double Ma,double T,double AOA);
+#define Velocity_Inlet_Undersonic(u,v,T)
+    extern void velocity_inlet_undersonic_boundary(double u,double v,double T);
+#define Pressure_Outlet(p,T_stg)
+    extern void pressure_outlet_boundary(double p,double T_stg);
+#define OT_flowfield(eachstep)
+    extern void output_flowfield(int eachstep);
